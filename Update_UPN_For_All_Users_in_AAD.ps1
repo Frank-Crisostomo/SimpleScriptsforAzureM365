aa6abcf63cd67e372997e558a2972252ad5b6f48
Francis Crisostomo <winmaster45@hotmail.com>
2023-02-07 19:38:02 -0500

    Update_UPN_For_All_Users_in_AAD.ps1

--- Update_UPN_For_All_Users_in_AAD.ps1
+++ Update_UPN_For_All_Users_in_AAD.ps1
@@ -5,7 +5,7 @@
 # Install AzureAD Module
 Install-Module AzureAD
 
-# Connect to AzureAD
+# Connect to AzureAD        
 Connect-AzureAD
 
 # Variables for old and new domains
@@ -14,28 +14,28 @@ $NewDomain = "contoso.onmicrosoft.com"
 
 # Check that new domain exist
 $GetDomain = Get-AzureADDomain -Name $NewDomain
-$DefaultDomain = $GetDomain.Name
+    If ($GetDomain){Write-Host "Domain" $NewDomain "exists" -ForegroundColor Green} Else {Write-Host "Domain" $NewDomain "does not exist" -ForegroundColor Red}
 
 # Get all AzureAD Users that are not Guest(B2B) Accounts
 $Users = Get-AzureADUser -All $true | Where-Object {($_.UserPrincipalName -notlike $NewDomain) -and ($_.UserType -ne 'Guest')}
 
 # For each user from the list of $Users
-Foreach($User in $Users){
+    Foreach($User in $Users){
 
-# Get there current UPN
-$OldUPN = $user.UserPrincipalName
+    # Get there current UPN
+    $OldUPN = $user.UserPrincipalName
 
-# Create the new UPN by replacing the domain in the variable $NewUPN
-$NewUPN = $user.UserPrincipalName -replace $OldDomain, $NewDomain
+    # Create the new UPN by replacing the domain in the variable $NewUPN
+    $NewUPN = $user.UserPrincipalName -replace $OldDomain, $NewDomain
 
-# Optional output
-Write-Host $user.UserPrincipalName "Updating to" $NewUPN -ForegroundColor Yellow
+    # Optional output
+    Write-Host $user.UserPrincipalName "Updating to" $NewUPN -ForegroundColor Yellow
 
-# Set $NewUPN
-Set-AzureADUser -UserPrincipalName $NewUPN
+    # Set $NewUPN
+    Set-AzureADUser -UserPrincipalName $NewUPN
 
-# Check that UPN has been updated
-$UpdatedUser = Get-AzureADUser -ObjectId $NewUPN
- If ($UpdatedUser){Write-Host $OldUPN "Updated to" $UpdatedUser.UserPrincipalName -ForegroundColor Green}
-
+    # Check that UPN has been updated
+    $UpdatedUser = Get-AzureADUser -ObjectId $NewUPN
+    If ($UpdatedUser){Write-Host $OldUPN "Updated to" $UpdatedUser.UserPrincipalName -ForegroundColor Green}
+    Else {Write-Host $OldUPN "Failed to update" -ForegroundColor Red}
 }
