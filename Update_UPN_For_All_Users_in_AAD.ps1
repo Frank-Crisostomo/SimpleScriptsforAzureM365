8c4486c0ace9daf67d2593f9389ee438d9090c1c
Francis Crisostomo <winmaster45@hotmail.com>
2023-02-08 07:50:54 -0500

    Update to 1.2.8.23

--- Update_UPN_For_All_Users_in_AAD.ps1
+++ Update_UPN_For_All_Users_in_AAD.ps1
@@ -3,6 +3,33 @@ Simple Script Series: Update UPN for All Users in Azure AD Tenant
 Created by Frank Crisostomo 2/6/2023
 #>
 
+<#
+  .SYNOPSIS
+  Simple Script Series: Update UPN for All Users in Azure AD Tenant
+
+  .DESCRIPTION
+ This script updates the UPN for all users in an Azure AD Tenant. This script is part of the Simple Script Series. The Simple Script Series is a collection of scripts that perform simple tasks. The scripts are designed to be easy to understand and modify.
+
+  .PARAMETER InputPath
+  None.
+
+  .PARAMETER OutputPath
+
+  .INPUTS
+  None. You cannot pipe objects to Update_UPN_For_All_Users_in_AAD.ps1.
+
+  .OUTPUTS
+  None.
+
+  .EXAMPLE
+  PS> .\Update_UPN_For_All_Users_in_AAD.ps1
+
+    .NOTES
+    Version:        1.2.8.23
+    Simple Script Series: Update UPN for All Users in Azure AD Tenant
+    Updated by Frank Crisostomo 2/7/2023
+#>
+
 # Install AzureAD Module
 Install-Module AzureAD
 
@@ -39,4 +66,9 @@ $Users = Get-AzureADUser -All $true | Where-Object {($_.UserPrincipalName -notli
     $UpdatedUser = Get-AzureADUser -ObjectId $NewUPN
     If ($UpdatedUser){Write-Host $OldUPN "Updated to" $UpdatedUser.UserPrincipalName -ForegroundColor Green}
     Else {Write-Host $OldUPN "Failed to update" -ForegroundColor Red}
-}
+    }
+
+# Disconnect from AzureAD
+Disconnect-AzureAD
+
+# End of Script
