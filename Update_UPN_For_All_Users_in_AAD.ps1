7319c207bf9fe5cc683fcfefd846b00a9dbcc8d0
@FrankCrisostomo <56240103+Frank-Crisostomo@users.noreply.github.com>
2023-02-16 10:43:04 -0500

    Update Update_UPN_For_All_Users_in_AAD.ps1

--- Update_UPN_For_All_Users_in_AAD.ps1
+++ Update_UPN_For_All_Users_in_AAD.ps1
@@ -1,35 +1,29 @@
 <#
-  .SYNOPSIS
-  Simple Script Series: Update UPN for All Users in Azure AD Tenant.
+.SYNOPSIS
+Simple Script Series: Update UPN for All Users in Azure AD Tenant.
+.DESCRIPTION
+This script updates the UPN for all users in an Azure AD Tenant. This script is part of the Simple Script Series. The Simple Script Series is a collection of scripts that perform simple tasks. The scripts are designed to be easy to understand and modify.
+.PARAMETER InputPath
+None.
+.PARAMETER OutputPath
+.INPUTS
+None. You cannot pipe objects to Update_UPN_For_All_Users_in_AAD.ps1.
+.OUTPUTS
+None.
+.EXAMPLE
+PS> .\Update_UPN_For_All_Users_in_AAD.ps1
+.NOTES
+Scenario: Update UPN for All Users in Azure AD Tenant after adding a new domain to Azure AD.
 
-  .DESCRIPTION
- This script updates the UPN for all users in an Azure AD Tenant. This script is part of the Simple Script Series. The Simple Script Series is a collection of scripts that perform simple tasks. The scripts are designed to be easy to understand and modify.
-
-  .PARAMETER InputPath
-  None.
-
-  .PARAMETER OutputPath
-
-  .INPUTS
-  None. You cannot pipe objects to Update_UPN_For_All_Users_in_AAD.ps1.
-
-  .OUTPUTS
-  None.
-
-  .EXAMPLE
-  PS> .\Update_UPN_For_All_Users_in_AAD.ps1
-
-    .NOTES
-    Scenario: Update UPN for All Users in Azure AD Tenant after addding a new domain to Azure AD.
-    
-    Version:        1.2.8.23
-    Simple Script Series: Update UPN for All Users in Azure AD Tenant
-    Updated by Frank Crisostomo 2/8/2023
+Version: 1.2.8.23
+Simple Script Series: Update UPN for All Users in Azure AD Tenant
+Updated by Frank Crisostomo 2/8/2023
 #>
 
 # Checks module, if missing it installs
-$CheckModule = Get-Module AzureAD
-if (!$CheckModule){Install-Module AzureAD}
+if (!(Get-Module AzureAD)) {
+    Install-Module AzureAD
+}
 
 # Connect to AzureAD        
 Connect-AzureAD
@@ -38,33 +32,39 @@ Connect-AzureAD
 $NewDomain = "contoso.com"
 $OldDomain = "contoso.onmicrosoft.com"
 
-# Check that new domain exist
-$GetDomain = Get-AzureADDomain -Name $NewDomain
-    If ($GetDomain){Write-Host "Domain" $NewDomain "exists" -ForegroundColor Green} Else {Write-Host "Domain" $NewDomain "does not exist" -ForegroundColor Red}
+# Check that new domain exists
+if (Get-AzureADDomain -Name $NewDomain) {
+    Write-Verbose "Domain $NewDomain exists"
+} else {
+    Write-Error "Domain $NewDomain does not exist"
+}
 
-# Get all AzureAD Users that are not guests Accounts such as B2B Accounts
-$Users = Get-AzureADUser -All $true | Where-Object {($_.UserPrincipalName -notlike $NewDomain) -and ($_.UserType -ne 'Guest')}
+# Get all AzureAD Users that are not guest accounts
+$Users = Get-AzureADUser -All $true | Where-Object {!($_.UserPrincipalName -contains $NewDomain) -and ($_.UserType -ne 'Guest')}
 
-    # For each user from the list of $Users
-    Foreach($User in $Users){
-
-    # Get there current UPN
-    $OldUPN = $user.UserPrincipalName
+# For each user from the list of $Users
+foreach ($User in $Users) {
+    # Get their current UPN
+    $OldUPN = $User.UserPrincipalName
 
     # Create the new UPN by replacing the domain in the variable $NewUPN
-    $NewUPN = $user.UserPrincipalName -replace $OldDomain, $NewDomain
+    $NewUPN = $OldUPN -replace $OldDomain, $NewDomain
 
-    # Optional output
-    Write-Host $user.UserPrincipalName "Updating to" $NewUPN -ForegroundColor Yellow
+    # Output the update
+    Write-Verbose "Updating $OldUPN to $NewUPN"
 
-    # Set $NewUPN
-    Set-AzureADUser -ObjectId $oldUPN -UserPrincipalName $NewUPN
+    # Set the new UPN
+    Set-AzureADUser -ObjectId $OldUPN -UserPrincipalName $NewUPN
 
-    # Checks that UPN has been updated
-    $UpdatedUser = Get-AzureADUser -ObjectId $NewUPN
-    If ($UpdatedUser){Write-Host $OldUPN "Updated to" $UpdatedUser.UserPrincipalName -ForegroundColor Green} Else {Write-Host $OldUPN "Failed to update" -ForegroundColor Red}}
+    # Check that UPN has been updated
+    if (Get-AzureADUser -ObjectId $NewUPN) {
+        Write-Verbose "$OldUPN updated to $NewUPN"
+    } else {
+        Write-Error "$OldUPN failed to update"
+    }
+}
 
 # Disconnect from AzureAD
 Disconnect-AzureAD
 
-# End of Script
+# End of script
